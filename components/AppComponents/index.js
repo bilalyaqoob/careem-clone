a196f36bd46f76894437009c1da3fc94bd94e693
Your Name <you@example.com>
2019-08-22 17:12:38 +0500

    first commit

---
+++ components/AppComponents/index.js
@@ -0,0 +1,6 @@
+// import TestComponent from './TestComponent';
+import EmailTextInput from './EmailTextInput';
+import PasswordTextInput from './PasswordTextInput';
+import PhoneButton from './PhoneButton';
+
+export { TestComponent, EmailTextInput, PasswordTextInput, PhoneButton };
